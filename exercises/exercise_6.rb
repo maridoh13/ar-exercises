6723cc99dfef1faf1ecc085f59d898d9516fbe5b
maridoh13 <maridoh@gmail.com>
2020-03-31 04:05:04 +0000

    done?

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,24 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
+# Exercise 6: One-to-many association
+# We haven't used the Employee class (and employees table) at all yet. If you look at this table's column structure, you'll find that it has a store_id (integer) column. This is a column that identifies which store each employee belongs to. It points to the id (integer) column of their store.
+
+# Let's tell Active Record that these two tables are in fact related via the store_id column.
+
+# Add the following code directly inside the Store model (class): has_many :employees
+# Add the following code directly inside the Employee model (class): belongs_to :store
+# Add some data into employees. Here's an example of one (note how it differs from how you create stores): @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+# Go ahead and create some more employees using the create method. You can do this by making multiple calls to create (like you have before.) No need to assign the employees to variables though. Create them through the @store# instance variables that you defined in previous exercis
+
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Joan", last_name: "Jett", hourly_rate: 60)
+@store1.employees.create(first_name: "Britney", last_name: "Spears", hourly_rate: 20)
+@store1.employees.create(first_name: "Lady", last_name: "Gaga", hourly_rate: 45)
+@store1.employees.create(first_name: "Christina", last_name: "Aguilera", hourly_rate: 50)
+
+@store2.employees.create(first_name: "Bryan", last_name: "Adams", hourly_rate: 50)
+@store2.employees.create(first_name: "Daryl", last_name: "Hall", hourly_rate: 45)
+@store2.employees.create(first_name: "John", last_name: "Oates", hourly_rate: 30)
+@store2.employees.create(first_name: "David", last_name: "Bowie", hourly_rate: 60)
